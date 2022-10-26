c84925dfda307f8fd869d494e85c9277bb4713d7
waready <elbarto119@hotmail.com>
2022-10-25 21:05:07 -0500

    final

--- config/database.js
+++ config/database.js
@@ -6,8 +6,8 @@ const Env = use('Env')
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
 
-const Url = require('url-parse')
-const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
+// const Url = require('url-parse')
+// const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
 
 module.exports = {
   /*
@@ -73,6 +73,17 @@ module.exports = {
   | npm i --save pg
   |
   */
+  // pg: {
+  //   client: 'pg',
+  //   connection: {
+  //     host: Env.get('DB_HOST', 'localhost'),
+  //     port: Env.get('DB_PORT', ''),
+  //     user: Env.get('DB_USER', 'root'),
+  //     password: Env.get('DB_PASSWORD', ''),
+  //     database: Env.get('DB_DATABASE', 'adonis')
+  //   },
+  //   debug: Env.get('DB_DEBUG', false)
+  // }
   pg: {
     client: 'pg',
     // connection: {
