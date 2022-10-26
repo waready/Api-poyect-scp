87586e8a840d8f061b19eb0655e816a155500dd7
waready <elbarto119@hotmail.com>
2022-10-25 21:02:25 -0500

    make it better2

--- config/database.js
+++ config/database.js
@@ -75,13 +75,18 @@ module.exports = {
   */
   pg: {
     client: 'pg',
+    // connection: {
+    //   host: Env.get('DB_HOST', 'localhost'),
+    //   port: Env.get('DB_PORT', ''),
+    //   user: Env.get('DB_USER', 'root'),
+    //   password: Env.get('DB_PASSWORD', ''),
+    //   database: Env.get('DB_DATABASE', 'adonis')
+    // }
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
-    },
-    debug: Env.get('DB_DEBUG', false)
+      connectionString:Env.get('DATABASE_URL',''),
+      ssl:{
+        rejectUnauthorized:false
+      }
+    }
   }
 }
